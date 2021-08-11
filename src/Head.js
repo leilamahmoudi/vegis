4e1372cfd51a8a39e5026227fa782a8bb0916846
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2021-08-11 21:12:00 +0200

    Added some css navbar

--- src/Head.js
+++ src/Head.js
@@ -1,29 +1,38 @@
 import React from "react";
+import "./Head.css";
 
 export default function Head() {
   return (
     <div className="Head">
       <div className="container">
-        <img src="/" alt="logo" />
-        <i class="fas fa-shopping-basket"></i>
+        <div className="header">
+          {/* <img className="header-logo" src="./images/download.png" alt="logo" /> */}
+          <i class="fas fa-seedling header-logo"></i>
+          <i class="fas fa-shopping-basket header-basket"></i>
+        </div>
+
         <nav>
-          <form>
-            <select>
-              <option value="browser category">Browser category</option>
-              <option value="breakfast">Breakfast</option>
-              <option value="dinner">Dinner</option>
-              <option value="lunch">Lunch</option>
-              <option value="cheese">Cheese</option>
-            </select>
+          <form className="nav-header">
+            <div className="nav-category">
+              <select>
+                <option value="browser category">Browser category</option>
+                <option value="breakfast">Breakfast</option>
+                <option value="dinner">Dinner</option>
+                <option value="lunch">Lunch</option>
+                <option value="cheese">Cheese</option>
+              </select>
+            </div>
+            <div className="header-home-item">
+              <li>
+                <ul>HOME</ul>
+                <ul>SHOP</ul>
+                <ul>COLLECTION</ul>
+                <ul>PAGES</ul>
+                <ul>BLOGS</ul>
+                <ul>BUY VIGIST</ul>
+              </li>
+            </div>
           </form>
-          <li>
-            <ul>HOME</ul>
-            <ul>SHOP</ul>
-            <ul>COLLECTION</ul>
-            <ul>PAGES</ul>
-            <ul>BLOGS</ul>
-            <ul>BUY VIGIST</ul>
-          </li>
           <i class="fas fa-headset"></i>
           <ul>
             <li>
